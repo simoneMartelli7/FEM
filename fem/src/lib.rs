a2e50984c61e6b98630ccebf2e44209af2ac66ae
Simone Martelli <simone.martelli@pm.me>
2022-05-15 20:47:51 +0200

    created supports struct

--- fem/src/lib.rs
+++ fem/src/lib.rs
@@ -20,7 +20,7 @@ use std::io;
 }
 */
 
-fn read_input() -> String {
+pub fn read_input() -> String {
     let mut buffer = String::new();
     io::stdin().
         read_line(&mut buffer).
@@ -28,13 +28,13 @@ fn read_input() -> String {
     buffer
 }
 
-struct Supports {
+pub struct Supports {
     model: String,
     vin: Vec<String>,
     position: f32,
 }
 impl Supports {
-    pub fn new(&mut self) {
+    pub fn new(&mut self) -> Supports {
         let mut option: u8;
         let mut postion: f32;
 
@@ -59,6 +59,7 @@ impl Supports {
                 self.model = String::from("Incastro");
                 self.vin = vec![String::from("Vertical"), String::from("Horizontal"), String::from("Rotation")];
             }
+            _ => {println!("Please insert a valid number!"); /*self:new() not working check reddit post on laptop*/}
         }
 
         println!("In what position would you like to insert it? \n(In fraction of the length or x/L)");
@@ -67,7 +68,11 @@ impl Supports {
     }
 }
 
-
+/* TODO: 1. check reddit post on laptop for supports::new() [recursion]
+         2. fix rod and supports ::new, maybe implement a Copy trait
+         3. function to apply loads
+         4. function to compute forces applied by the supports
+ */
 
 
 pub struct Rod {
@@ -78,7 +83,7 @@ pub struct Rod {
      */
     length: u32,
     alpha: f32,
-    supports: struct,
+    supports: Supports,
 }
 impl Rod {
     pub fn new(&mut self) -> Rod {
@@ -91,7 +96,9 @@ impl Rod {
         print!("\nAlpha: ");
         let a: f32 = read_input().parse().unwrap();
 
-        Rod { length: l, alpha: a, supports: self.supports }
+        let mut supports = Supports::new();
+
+        Rod { length: l, alpha: a, supports: self.supports = supports}
     }
     //changes the length of the rod
     pub fn change_l(&mut self, new_length: u32) {
