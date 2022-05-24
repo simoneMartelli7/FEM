6d0e1700370f3718cfe64c83cfdd06faace64f78
Simone Martelli <simone.martelli@pm.me>
2022-05-24 18:54:45 +0200

    implemented constrained degree of freedom, need to implement K matrix

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,20 +1,22 @@
-use std::collections::HashMap;
 use std::convert::TryFrom;
 extern crate nalgebra as na;
 /* This is a test as a proof of concept. It's a hard coded implementation of a FEM analysis on a
    single rod element
  */
 
+
+
 fn main() {
     //length of the rod
     let l: f32 = 1.500;
 
-    /* supports:
-        the first element is a String and represent the type of support
-        the second one represent where it's located
-     */
-    let mut p: HashMap<String, f32> = HashMap::new();
-    p.insert(String::from("Vertical"), 0.0);
+    // this are the vectors for the horizontal and vertical constraint respectively
+    //working on a better solution
+    let mut h: Vec<f32> = Vec::new();
+    let mut v: Vec<f32> = Vec::new();
+
+    h.push(0.0);
+
     //size of the mesh: this is the number of nodes which is number of elements + 1
     const N: usize = 7;
     //this creates a vector containing only zeros where the position of the various nodes will go
@@ -33,7 +35,7 @@ fn main() {
 
     //some rod properties
     let E: f32 = 73000.0;
-    let S: f32 = 0.050;
+    let A: f32 = 0.050;
 
     //this next snippet of code will be the function which will create the mesh
     let n = u16::try_from(N).unwrap(); //disposable N for iterator
@@ -42,41 +44,42 @@ fn main() {
 
 
     // this matrix contains in each row area and material properties of each element
-    let mut cpe: na::SMatrix<f32, N, 2> = na::SMatrix::zero();
+    let mut cpe: na::SMatrix<f32, N, 2> = na::SMatrix::zeros();
 
     for i in 0..n {
         let i_f32 = f32::try_from(i).unwrap();
         let mut i_u: usize = i as usize;
         mesh[i_u] = i_f32*basic_element;
-        cpe[(i_u, 0)] = S;
-        cpe[(i_u, 2)] = E;
+        cpe[(i_u, 0)] = A;
+        cpe[(i_u, 1)] = E;
     }
 
     //being a single horizontal rod element all the nodes are located on a line  with coordinates
     //given by the mesh vector. In a more rigorous way a connectivity matrix would need to be
     //implemented
-    for (key, value) in &p {
-        //needed to fix the dimension of ib in order to not have a dynamic vector
-        //in this case may be more convoluted, but in more realistic cases the memory saving may be
-        //worth the few line of codes added
-        println!("");
 
+    let temp_s: usize = h.len() + v.len();
+    const S: usize = 1;
+
+    //this is the vector of the degrees of freedom which are constrained and so are 0
+    let mut ib: na::SVector<f32, S> = na::SVector::zeros();
+    let mut counter: usize = 0;
+    for i in h.iter() {
+        ib[counter] = (i + 1.0) * 2.0;
+        counter += 1;
     }
-    let mut ib: na::SVector<f32, N> = na::SVector::zeros();
-    for (key, value) in &p {
-        match key {
-            String::from("Vertical") => {
-                for i in 0..n {
-                    let mut i_u: usize = i as usize;
-                    if mesh[i_u] == *value {
-                        println!("okay")
-                    }
-                }
-            }
-            String::from("Horizontal") => {
-                println!("okay")
-            }
-            _ => println!("please insert at least one (1) valid constraint")
-        }
+    for i in v.iter() {
+        ib[counter] = ( i + 1.0 ) * 2.0 - 1.0;
+        counter += 1;
     }
+
+    if counter != S {panic!("Critical Error: please restart program")}
+
+
+    let mut Fn: na::SMatrix<f32, 1, 2> = na::SMatrix::zeros();
+    Fn[(0, 0)] = 14.0;
+    Fn[(0, 1)] = -65000.0;
+
+    println!("{}", Fn)
+
 }
